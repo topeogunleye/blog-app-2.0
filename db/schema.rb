8c2031f6dec05385169aa47c30901b22c29aa573
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-01-19 20:48:39 +0100

    Update Schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_119_163_631) do
+ActiveRecord::Schema[7.0].define(version: 20_230_119_194_624) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -25,7 +25,6 @@ ActiveRecord::Schema[7.0].define(version: 20_230_119_163_631) do
   end
 
   create_table 'likes', force: :cascade do |t|
-    t.text 'text'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.bigint 'author_id', null: false
