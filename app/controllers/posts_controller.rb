38526fe0ff097482a1f675a1099de1622c8a892d
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-01-25 05:38:53 +0100

    Use latest posts and latest comments method from models

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all.order(id: :desc)
-    @latest_posts = Post.all.order(id: :desc).limit(5)
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.order(id: :desc)
-    @latest_posts = @user.posts.order(id: :desc).limit(5)
   end
 end
