6a7cef7997b88d48cc85ff6561a1777faf9a69e8
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-01-26 21:34:17 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'users#index'
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-  get '/users/:user_id/posts', to: 'posts#index'
-  get 'users/:user_id/posts/:id', to: 'posts#show'
-  get 'users/new', to: 'users#new'
-  post 'users', to: 'users#create'
-  get 'users/:id/edit', to: 'users#edit'
-  patch 'users/:id', to: 'users#update'
-  delete 'users/:id', to: 'users#destroy'
-  get 'users/:user_id/posts/new', to: 'posts#new'
-  post 'users/:user_id/posts', to: 'posts#create'
-  get 'users/:user_id/posts/:id/edit', to: 'posts#edit'
-  patch 'users/:user_id/posts/:id', to: 'posts#update'
-  delete 'users/:user_id/posts/:id', to: 'posts#destroy'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy]
+    resources :comments, only: [:new, :create, :destroy]
+    resources :likes, only: [:new, :create]
+  end
 end
