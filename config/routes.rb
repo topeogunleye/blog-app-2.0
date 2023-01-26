a3c190a6229296333922e100b0786604e9f2ced7
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-01-26 13:41:57 +0100

    Generate comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+  get 'comments/create'
+  get 'comments/edit'
+  get 'comments/update'
+  get 'comments/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
