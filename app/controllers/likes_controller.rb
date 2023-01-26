a51a7d0d441663b7644eaa529bc6c91f39ecf409
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-01-26 21:36:27 +0100

    Add actions to likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,11 @@
 class LikesController < ApplicationController
+  def def(_new)
+    @like = Like.new
+  end
+
   def create
+    # new object from params
+    Like.create(author_id: params[:user_id], post_id: params[:post_id])
+    redirect_to user_post_url(id: params[:post_id])
   end
 end
