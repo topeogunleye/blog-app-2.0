ca64e302c0cfc5a4e4e2662d23ee41f3e9faccb4
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-01-31 07:24:30 +0100

    Fix linters

--- spec/features/post_index_page_spec.rb
+++ spec/features/post_index_page_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-
-RSpec.describe "User", type: :feature do
+RSpec.describe 'User', type: :feature do
   before :each do
     first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-    second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+    second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                              bio: 'Teacher from Poland.')
     first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
-    Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
-    Comment.create(post: first_post, author: second_user, text: 'Hello Tom!' )
+    Comment.create(post: first_post, author: second_user, text: 'Hi Tom!')
+    Comment.create(post: first_post, author: second_user, text: 'Hello Tom!')
     visit user_posts_path(first_user)
   end
 
-  describe "index page" do
+  describe 'index page' do
     it "I can see the user's profile picture" do
       all_images = page.all('img')
       all_images.each do |img|
@@ -19,24 +19,23 @@ RSpec.describe "User", type: :feature do
         expect(link).to eq('/assets/img2-3b2280af3d478258894911b061e6c8002e3a687c7ad0568da200cb39dc5e35f4.jpg')
       end
     end
-  
+
     it "I can see the user's username" do
       expect(page).to have_content('Tom')
       expect(page).to have_content('Lilly')
     end
-  
-    it "I can see the number of posts the user has written" do
+
+    it 'I can see the number of posts the user has written' do
       expect(page).to have_content('Number of posts: 1')
     end
-  
+
     it "I can see a post's title" do
       expect(page).to have_content('Hello')
     end
-  
-    it "I can see the first 3 comments on a post" do
-      # Add code to create and display first 3 posts
-      expect(page).to have_content('Hi Tom!')  
-    end
 
+    it 'I can see the first 3 comments on a post' do
+      # Add code to create and display first 3 posts
+      expect(page).to have_content('Hi Tom!')
+    end
   end
 end
--- spec/features/user_index_page_spec.rb
+++ spec/features/user_index_page_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-
-RSpec.describe "User", type: :feature do
+RSpec.describe 'User', type: :feature do
   before :each do
     first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-    second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+    second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                              bio: 'Teacher from Poland.')
     first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
-    Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
-    Comment.create(post: first_post, author: second_user, text: 'Hello Tom!' )
+    Comment.create(post: first_post, author: second_user, text: 'Hi Tom!')
+    Comment.create(post: first_post, author: second_user, text: 'Hello Tom!')
     visit users_path
   end
 
-  describe "index page" do
+  describe 'index page' do
     it "I can see the user's profile picture" do
       all_images = page.all('img')
       all_images.each do |img|
@@ -19,27 +19,26 @@ RSpec.describe "User", type: :feature do
         expect(link).to eq('https://unsplash.com/photos/F_-0BxGuVvo')
       end
     end
-  
+
     it "I can see the user's username" do
       visit users_path
       expect(page).to have_content('Tom')
       expect(page).to have_content('Lilly')
     end
-  
-    it "I can see the number of posts the user has written" do
+
+    it 'I can see the number of posts the user has written' do
       visit users_path
       expect(page).to have_content('Number of Posts: 1')
     end
-  
+
     it "I can see the user's bio" do
       visit users_path
       expect(page).to have_content('Teacher from Mexico.')
       expect(page).to have_content('Teacher from Poland.')
     end
-  
+
     it "I can see the user's first 3 posts" do
       # Add code to create and display first 3 posts
     end
-
   end
 end
