a7b9a9e6c6a74992afe17b96f317934081f915aa
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-02-02 17:15:03 +0100

    Fix ability class

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -31,7 +31,7 @@ class Ability
     return unless user.present?
 
     can :manage, Post, user_id: user.id # if the user is logged in can manage it's own posts
-    can :manage, Comment, user_id: user.id # logged in users can also create comments
+    can :manage, Comment, author_id: user.id # logged in users can also create comments
 
     return unless user.is? :admin
 
